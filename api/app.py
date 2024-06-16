1dda9c68622d833279cea2b904302c72d1965ac7
Eber Junior <eberssjunior12@gmail.com>
2024-06-16 16:43:06 -0300

    Atualizando o portfólio de acordo com as atividades do segundo semestre

--- api/app.py
+++ api/app.py
@@ -12,11 +12,17 @@ def home():
 
 @app.route('/academico')
 def academico():
-    projetos = [{"nome": "Portfólio Digital", "img": "../static/img/img-portfolio-digital500.png", "link": "https://github.com/eberssj/portfolio_digital_dsm"}, 
-    {"nome": "Desafio - DevWeb I", "img": "../static/img/speaknow500.png", "link": "https://github.com/eberssj/desafio-speaknow"}, 
-    {"nome": "API II - Nefrologia", "img": "../static/img/img-api500.png", "link": "https://github.com/TeamHiveAPI/API-2023.2"}]
+    projetos = [
+        {"nome": "Portfólio Digital", "img": "../static/img/img-portfolio-digital500.png", "link": "https://github.com/eberssj/portfolio_digital_dsm"}, 
+        {"nome": "Desafio - DevWeb I", "img": "../static/img/speaknow500.png", "link": "https://github.com/eberssj/desafio-speaknow"}, 
+        {"nome": "API 2023.2 - Nefrologia", "img": "../static/img/img-api500.png", "link": "https://github.com/TeamHiveAPI/API-2023.2"},
+        {"nome": "DevWeb II", "img": "../static/img/devwebii500.png", "link": "https://github.com/eberssj/devwebII"},
+        {"nome": "API 2024.1 - Woodpecker", "img": "../static/img/api2024-2-500.png", "link": "https://github.com/TeamHiveAPI/API-2024.01"},
+        {"nome": "WorldBeauty", "img": "../static/img/wb500.png", "link": "https://github.com/eberssj/wb"}
+    ]
     title = "Acadêmico"
     return render_template("academico.htm", title=title, projetos=projetos)
+
 
 
 @app.route('/sobre')
